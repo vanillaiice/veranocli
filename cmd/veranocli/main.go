a284c4fc52c7131c8bf86b3ca636a5594386f46d
vanilla <vanillaiice@tutanota.com>
2024-01-22 16:03:20 +0300

    chore: bump version to v0.0.6

--- cmd/veranocli/main.go
+++ cmd/veranocli/main.go
@@ -12,7 +12,7 @@ func main() {
 	app := &cli.App{
 		Name:    "verano cli",
 		Suggest: true,
-		Version: "v0.0.1",
+		Version: "v0.0.6",
 		Authors: []*cli.Author{{Name: "Vanillaiice", Email: "vanillaiice1@proton.me"}},
 		Usage:   "manage activities in a project",
 		Commands: []*cli.Command{
